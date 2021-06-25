3e47e3b22e75dd335bcc5acc97904b0dde6db720
Erika Infante <erikainfantedoo@gmail.com>
2021-06-24 21:55:18 -0500

    responsive

--- web-browser/src/components/ItemCard/index.tsx
+++ web-browser/src/components/ItemCard/index.tsx
@@ -13,20 +13,22 @@ const ItemCard: React.FC<item> = ({ data }) => {
         <Link className="Link" to={`items/${data.id}`}>
           <div className="imgContent" style={{ backgroundImage: `url(${data.thumbnail})` }}></div>
         </Link>
-        <div className="infoContent">
-          <p className="price">
-            ${' '}
-            {data.price
-              .toFixed(0)
-              .toString()
-              .replace(/\B(?=(\d{3})+(?!\d))/g, '.')}
-          </p>
-          <Link className="Link" to={`items/${data.id}`}>
-            <h2>{data.title}</h2>
-          </Link>
-        </div>
-        <div className="place">
-          <p>{data.address.state_name}</p>
+        <div className="ItemText">
+          <div className="infoContent">
+            <p className="price">
+              ${' '}
+              {data.price
+                .toFixed(0)
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, '.')}
+            </p>
+            <Link className="Link" to={`items/${data.id}`}>
+              <h2>{data.title}</h2>
+            </Link>
+          </div>
+          <div className="place">
+            <p>{data.address.state_name}</p>
+          </div>
         </div>
       </div>
       <hr />
